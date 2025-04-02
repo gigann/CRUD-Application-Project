44ec227551c08f43f579f449f9263950085ca6a5
gigann <issacgann@gmail.com>
2025-04-02 13:01:25 +0200

    added more front-end scaffolding and added contexts for login and theme

---
+++ client/src/components/InventoryItemCard/InventoryItemCard.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+import './InventoryItemCard.css';
+
+function InventoryItemCard() {
+
+    return (
+        <>
+            <h1>Inventory Item Card</h1>
+        </>
+    )
+}
+
+export default InventoryItemCard;
--- client/src/components/InventoryPage/InventoryPage.jsx
+++ client/src/components/InventoryPage/InventoryPage.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './InventoryPage.css';
+import InventoryItemCard from '../InventoryItemCard/InventoryItemCard';
 
 function InventoryPage() {
+    const navigate = useNavigate();
 
     return (
         <>
+            <nav>
+                <button onClick={() => navigate(-1)}>Go Back</button>
+                <button className = 'edit-button'>Edit</button>
+            </nav>
+
             <h1>Inventory</h1>
+
+            {/* <InventoryItemCard></InventoryItemCard>
+            <InventoryItemCard></InventoryItemCard>
+            <InventoryItemCard></InventoryItemCard> */}
         </>
     )
 }
