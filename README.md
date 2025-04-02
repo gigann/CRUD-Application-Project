cbbfd592e7166ff57502e81357aede0cfe17391c
gigann <issacgann@gmail.com>
2025-04-02 21:23:36 +0200

    updated dummy usernames/passwords and readme

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ TODO:
 
 Instructions for spinning up / setting up connection string
 
+Clone or download and extract the zip file.
+
+
+
+//client
+front end: npm start
+
+//server
+back end: npm start
+
+//database
+
+
+Dummy data
+You can login as Mario, Steve, and Simon.
+Here are their usernames and passwords:
+Mario
+username: itsame
+password: MAR10
+
+Steve
+username: mc_master
+password: 12345
+
+Simon
+username: vampire_killer
+password: Wallachia1476!
--- server/seeds/1_user.js
+++ server/seeds/1_user.js
@@ -2,8 +2,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('user_account').del()
   await knex('user_account').insert([
-    { first_name: 'Mario', last_name: 'Mario', username: 'itsame', password: '$2b$12$GD7X2Dq.FKXHyJlEmVLgVOvP/khI5QRUxbb4o3y2kzIxHuP6b7XFq'},
-    { first_name: 'Minecraft', last_name: 'Steve', username: 'mc_master', password: '$2b$12$Uto.lQg6OQlunLwDG9VSb.c6/2DPBOyP.v5IsbypWPg0kKpZdpMjm' },
-    { first_name: 'Simon', last_name: 'Belmont', username: 'vampire_killer', password: '$2b$12$zxEEjSqqEEply00RHHgHUe5rIVXrc4NSuWeWsjYMhvAMpdfkSh9EO' },
+    { first_name: 'Mario', last_name: 'Mario', username: 'smbro', password: '$2b$12$kF50VQIIds/fgZtFCgvFSe4XDeN2oR/KoYlN/vyE.s1hYoI43KiUi'},
+    { first_name: 'Minecraft', last_name: 'Steve', username: 'mc_master', password: '$2b$12$Rp2svaHhFY/9LMLv83Hz8u7ET40ucOiGcqsUlPpNnl8pp393a7fFS' },
+    { first_name: 'Simon', last_name: 'Belmont', username: 'vampire_killer', password: '$2b$12$eebKoW4koXAteo94MDrZa.dtnRIMrJRaoL0qAKgWnA4eZdMREpwrK' },
   ]);
 };
