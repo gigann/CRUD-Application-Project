9d913ce38174f49fae5c54c00a5904c747698be1
gigann <issacgann@gmail.com>
2025-04-03 03:12:59 +0200

    added personal inventory page

--- client/src/components/UserInventoryPage/UserInventoryPage.jsx
+++ client/src/components/UserInventoryPage/UserInventoryPage.jsx
@@ -2,33 +2,53 @@ import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import './UserInventoryPage.css';
-import InventoryItemCard from '../InventoryItemCard/InventoryItemCard';
+import InventoryItemCard from '../InventoryItemCard/InventoryItemCard.jsx';
 import { UserContext } from '../../contexts/UserContext.jsx';
 
 
 function UserInventoryPage() {
     const [userID, setUserID] = useContext(UserContext);
 
+    const [usersItems, setUsersItems] = useState([]);
+
     const navigate = useNavigate();
 
     // displays a list of items made by the user
     useEffect(() => {
-        fetch(`http://localhost:1337/item/${userID}`)
+        fetch(`http://localhost:1337/item/user/${userID}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setUsersItems(data);
+            })
 
-    }, [userID])
+    }, [])
 
     return (
         <>
             <nav>
                 <button onClick={() => navigate(-1)}>Go Back</button>
-                <button className = 'edit-button'>Edit</button>
             </nav>
 
-            <h1>Inventory</h1>
+            {(userID > 0) ? (
+                <>
+                    <h1>My Inventory</h1>
+                    <button className='edit-button'>Edit</button>
 
-            {/* <InventoryItemCard></InventoryItemCard>
-            <InventoryItemCard></InventoryItemCard>
-            <InventoryItemCard></InventoryItemCard> */}
+                    {usersItems.map((item) => (
+                        <InventoryItemCard
+                            id={item.id}
+                            user_id={item.user_id}
+                            item_name={item.item_name}
+                            description={item.description}
+                            quantity={item.quantity}
+                        />
+                    ))}
+                </>
+            ) : (
+                <>
+                    <h1>Oops! You're not supposed to be here!</h1>
+                </>
+            )}
         </>
     )
 }
