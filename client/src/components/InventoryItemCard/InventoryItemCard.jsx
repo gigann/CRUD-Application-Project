c184e46551c487a3a36d114c2a8654dfe7cbd33d
gigann <issacgann@gmail.com>
2025-04-03 03:03:53 +0200

    implemented item details page

--- client/src/components/InventoryItemCard/InventoryItemCard.jsx
+++ client/src/components/InventoryItemCard/InventoryItemCard.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './InventoryItemCard.css';
+import ItemDetailsPage from '../ItemDetailsPage/ItemDetailsPage';
 
 // supplied with relevant info via prop drilling
+// when clicked, goes to item details page (for that specific item)
 function InventoryItemCard(children) {
+    const navigate = useNavigate();
 
     return (
-        <div id={children.id}>
+        <div id={children.id} onClick={(e) => {
+            navigate(`/inventory/${children.id}`, {state: children});
+        }}>
             <h3>{children.item_name} (x{children.quantity})</h3>
-            <p><i>{children.description}</i></p>
+            {(children.description.length > 100)?(
+                <p><i>{children.description.slice(0, 100)}...</i></p>
+            ):(
+                <p><i>{children.description}</i></p>
+            )}
             <p>Item ID: {children.id} | User ID: {children.user_id}</p>
         </div>
     )
--- client/src/components/ItemDetailsPage/ItemDetailsPage.jsx
+++ client/src/components/ItemDetailsPage/ItemDetailsPage.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './ItemDetailsPage.css';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function ItemDetailsPage() {
+    const navigate = useNavigate();
+    const location = useLocation();
 
     return (
         <>
-            <h1>Item Details</h1>
+            {(location.state) ? (
+                <>
+                    <h1>{location.state.item_name}</h1>
+                    <p>{location.state.description}</p>
+                    <p>Quantity: {location.state.quantity}</p>
+                    <p>Item ID: {location.state.id}</p>
+                    <p>User ID: {location.state.user_id}</p>
+                </>
+
+            ) : (
+                <><h1>Oops!</h1>
+                    <p>The item you are looking for doesn't exist.</p>
+                </>
+            )}
+
+            <nav>
+                <button onClick={() => navigate(-1)}>Go Back</button>
+            </nav>
         </>
     )
 }
