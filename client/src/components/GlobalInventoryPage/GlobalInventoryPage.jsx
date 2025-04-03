bf2d1b92b34e98d54f7d4d61a7fcfd5947ffd357
gigann <issacgann@gmail.com>
2025-04-03 12:08:03 +0200

    added a little css  to tables

--- client/src/components/GlobalInventoryPage/GlobalInventoryPage.jsx
+++ client/src/components/GlobalInventoryPage/GlobalInventoryPage.jsx
@@ -24,17 +24,30 @@ function GlobalInventoryPage() {
 
             <h1>Global Inventory</h1>
 
-            {allItems.map((item) => (
-                <InventoryItemCard
-                    key={item.id}
-                    id={item.id}
-                    user_id={item.user_id}
-                    item_name={item.item_name}
-                    description={item.description}
-                    quantity={item.quantity}
-                    editable={false}
-                />
-            ))}
+            <table>
+                <thead>
+                    <tr>
+                        <th scope='col'>ID</th>
+                        <th scope='col'>User ID</th>
+                        <th scope='col'>Item Name</th>
+                        <th scope='col'>Description</th>
+                        <th scope='col'>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allItems.map((item) => (
+                        <InventoryItemCard
+                            key={item.id}
+                            id={item.id}
+                            user_id={item.user_id}
+                            item_name={item.item_name}
+                            description={item.description}
+                            quantity={item.quantity}
+                            editable={false}
+                        />
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 }
